9d5fa8e6748dfb8de88e9b25522d8d29321a646a
Comandao <comandaoashleyken@gmail.com>
2023-12-06 21:39:31 +0800

    complete structure of about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ import Ryan from '../components/resources/founders/RyanY.jpg';
 import '../styles/about.css';
 
 const vision = "Our vision is to revolutionize the digital landscape by providing high-quality e-services that seamlessly connect company employers and Filipino college freelancers. We aim to empower businesses with cost-effective solutions starting at a low price of $5, fostering collaboration and mutual growth in the digital economy.";
-const mission = "At &copy; College Gigs, our mission is to bridge the gap between company employers and college freelancers by offering a diverse range of high-quality e-services. We are committed to providing a user-friendly platform that enables employers to access talented freelancers who can deliver exceptional results at an affordable price point. Through our dedication to excellence, innovation, and fostering a supportive community, we strive to empower freelancers to showcase their skills and help businesses thrive in the digital age.";
+const mission = "Our mission is to bridge the gap between company employers and college freelancers by offering a diverse range of high-quality e-services. We are committed to providing a user-friendly platform that enables employers to access talented freelancers who can deliver exceptional results at an affordable price point. Through our dedication to excellence, innovation, and fostering a supportive community, we strive to empower freelancers to showcase their skills and help businesses thrive in the digital age.";
 
 
 
@@ -25,12 +25,14 @@ function about() {
                     <img src={Ryan} alt='ryan' className='portrait ryan'/>
                 </div>
 
-                <div className='mission-vision'>
-                    <div className='mission'>
-                        <h1>hello</h1>
+                <div id='vm_section'>
+                    <div className="vm_card" id='vm_heading'>
+                        <h1 className="vm_card_item vision_heading">Vision</h1>
+                        <h1 className="vm_card_item mission_heading">Mission</h1>
                     </div>
-                    <div className='vision'>
-                        <h1>world</h1>
+                    <div className="vm_card" id='vm_content'>
+                        <p className="vm_card_item vision_content">{vision}</p>
+                        <p className="vm_card_item mission_content">{mission}</p>
                     </div>
                 </div>
 
@@ -39,5 +41,7 @@ function about() {
 
     )
 }
+
+
 
 export default about
