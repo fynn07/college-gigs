666e85c2335e8ce5dd4f276f5332ce9b9fc5c77e
Comandao <comandaoashleyken@gmail.com>
2023-12-06 19:46:00 +0800

    re-elevate the z-index of the navbar and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,34 +6,34 @@ import TwitterIcon from './resources/ico/twitter-sign.png'
 import InstagramIcon from './resources/ico/instagram.png'
 
 function footer() {
-  return (
-    <div className="container">
-        <div className="footer-links">
-            <div className="footer-links-link">
-            <Link to="/About">About</Link>
-            <Link to="/Contact">Contact Us</Link>
-            </div>         
-        </div>
-        <div className="rights-reserved">
-            <p>&copy; 2023 CollegeGigs. All rights reserved. </p>
-        </div>
-        <div className="social-media">
-            <div className='social-media-components'>
-                <a href='https://www.facebook.com' target='_blank' rel="noopener noreferrer" className='fb'>
-                    <img src={FacebookIcon} alt='facebook icon' className='sc facebook'/>
-                </a>
-                <a href='https://www.twitter.com' target='_blank' rel="noopener noreferrer" className='fb'>
-                    <img src={TwitterIcon} alt ='twitter icon' className='sc twitter'/>
-                </a>
-                <a href='https://www.instagram.com' target='_blank' rel="noopener noreferrer" className='fb'>
-                    <img src={InstagramIcon} alt='instagram icon' className='sc instagram'/>
-                </a>
+    return (
+        <div className="container" id="footer_container">
+            <div className="footer-links">
+                <div className="footer-links-link">
+                    <Link to="/About">About</Link>
+                    <Link to="/Contact">Contact Us</Link>
+                </div>
             </div>
+            <div className="rights-reserved">
+                <p>&copy; 2023 CollegeGigs. All rights reserved. </p>
+            </div>
+            <div className="social-media">
+                <div className='social-media-components'>
+                    <a href='https://www.facebook.com' target='_blank' rel="noopener noreferrer" className='fb'>
+                        <img src={FacebookIcon} alt='facebook icon' className='sc facebook'/>
+                    </a>
+                    <a href='https://www.twitter.com' target='_blank' rel="noopener noreferrer" className='fb'>
+                        <img src={TwitterIcon} alt ='twitter icon' className='sc twitter'/>
+                    </a>
+                    <a href='https://www.instagram.com' target='_blank' rel="noopener noreferrer" className='fb'>
+                        <img src={InstagramIcon} alt='instagram icon' className='sc instagram'/>
+                    </a>
+                </div>
 
-            
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default footer
