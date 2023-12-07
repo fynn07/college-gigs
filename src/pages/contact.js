386cb302381671159af16b4d4e2dfe7077a816d3
Comandao <comandaoashleyken@gmail.com>
2023-12-07 10:16:11 +0800

    add initial aesthetics of contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,12 +9,22 @@ function contact() {
                     <h1>Contact Us</h1>
                 </div>
                 <form id="contact_form" action="/College_Gigs/C_Gigs_Home.php" method="post">
-                    <label htmlFor="email"><i className="fa fa-envelope"></i> Email</label>
-                    <input type="email" id="email" name="email" required/>
-                    <label htmlFor="subject">Subject</label>
-                    <input type="text" id="subject" name="subject"/>
-                    <label htmlFor="desc"><i className="fa fa-commenting-o"></i> Description</label>
-                    <textarea id="desc" name="desc" className="desc" placeholder="Describe your issue here..."></textarea>
+                    <div className="contact_form_item">
+                        <label htmlFor="email"><i className="fa fa-envelope"></i> Email</label>
+                        <input type="email" id="email" name="email" className="contact_input" required/>
+                    </div>
+
+                    <div className="contact_form_item">
+                        <label htmlFor="subject">Subject</label>
+                        <input type="text" id="subject" name="subject" className="contact_input"/>
+                    </div>
+
+                    <div className="contact_form_item">
+                        <label htmlFor="desc"><i className="fa fa-commenting-o"></i> Description</label>
+                        <textarea id="desc" name="desc" className="desc contact_input"
+                                  placeholder="Describe your issue here..."></textarea>
+                    </div>
+
                     <button type="submit" name="send">Send Email</button>
                 </form>
             </div>
