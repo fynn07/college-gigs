286e1f9f319a58e37a672bf33e9b254efa05f86e
Comandao <comandaoashleyken@gmail.com>
2023-12-07 09:57:29 +0800

    adjust dimensions and structure of contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,24 +2,24 @@ import React from 'react'
 import '../styles/contact.css'
 
 function contact() {
-  return (
-    <div className='contact-container'>
-      <div className='header'>
-          <h1>Contact Us</h1>
-      </div> 
-      <div className='container-contact'>
-        <form action="/College_Gigs/C_Gigs_Home.php" method="post">
-          <label for="email"><i class="fa fa-envelope"></i> Email</label>
-          <input type="email" id="email" name="email" required/>
-          <label for="subject">Subject</label>
-          <input type="text" id="subject" name="subject"/>
-            <label for="desc"><i class="fa fa-commenting-o"></i> Description</label>
-            <textarea id="desc" name="desc" class="desc" placeholder="Describe your issue here..."></textarea>
-            <button type="submit" name="send">Send Email</button>
-        </form>
-      </div>
-    </div>
-  )
+    return (
+        <div id="contact">
+            <div id='contact_container'>
+                <div className="header" id='contact_header'>
+                    <h1>Contact Us</h1>
+                </div>
+                <form id="contact_form" action="/College_Gigs/C_Gigs_Home.php" method="post">
+                    <label htmlFor="email"><i className="fa fa-envelope"></i> Email</label>
+                    <input type="email" id="email" name="email" required/>
+                    <label htmlFor="subject">Subject</label>
+                    <input type="text" id="subject" name="subject"/>
+                    <label htmlFor="desc"><i className="fa fa-commenting-o"></i> Description</label>
+                    <textarea id="desc" name="desc" className="desc" placeholder="Describe your issue here..."></textarea>
+                    <button type="submit" name="send">Send Email</button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default contact
